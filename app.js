f5a008023f425146d1a74b428da95f31a95b79ad
Mohdsohail7 <sohailmohd437@gmail.com>
2025-01-23 19:35:25 +0530

    feat: i write all routes

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const { connectDB } = require("./db/init");
+
+const bookmarksRoutes = require("./routes/bookmarks");
+
 const app = express();
-require("dotenv").config();
-
 app.use(cors());
-
 app.use(express.json());
+
+// connect to database
+connectDB();
+
+// Routes
+app.use("/bookmarks", bookmarksRoutes)
 
 
 module.exports = { app }
